6cfc4bb945d315a80ac13bad067f151a7f6ba286
mezgoodle <mezgoodle@gmail.com>
2020-03-18 09:03:39 +0200

    Try to solve 4

--- Lab_rob_3/script.js
+++ Lab_rob_3/script.js
@@ -19,21 +19,18 @@ const
     colors = ['black', 'grey', 'brown', 'red', 'coral', 'chocolate', 'goldenrod', 'olive', 'yellow', 'lawngreen', 'darkgreen', 'teal', 'dodgerblue', 'navy', 'indigo', 'purple', 'crimson'],
 	
 	array = [
-		[0, 1, 1, 0],
-		[0, 0, 0, 1],
-		[0, 1, 0, 1],
-		[1, 1, 1, 0],
+		[0, 1, 1],
+		[1, 0, 1],
+		[1, 1, 0],
 	],
 
     graphs = [
 		[1, 2],
-        [1, 3],
-        [2, 4],
+		[1, 3],
+		[2, 1],
+		[2, 3],
+		[3, 1],
 		[3, 2],
-		[3, 4],
-		[4, 3],
-		[4, 1],
-		[4, 2],
     ],
 
     coords = {
