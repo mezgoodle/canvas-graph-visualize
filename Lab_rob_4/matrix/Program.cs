56cde65aae54b32b2dfdc9db15e6ee819ea1a93b
mezgoodle <mezgoodle@gmail.com>
2020-04-16 10:16:58 +0300

    Update Program.cs

--- Lab_rob_4/matrix/Program.cs
+++ Lab_rob_4/matrix/Program.cs
@@ -11,8 +11,10 @@ namespace matrix
                 Console.WriteLine(str);
                 for (int i = 0; i < n; i++)
                 {
+                    Console.Write("[");
                     for (int j = 0; j < n; j++)
-                        Console.Write(array[i, j] + " ");
+                        Console.Write(array[i, j] + ", ");
+                    Console.Write("],");
                     Console.WriteLine();
                 }
                 Console.WriteLine();
@@ -39,23 +41,13 @@ namespace matrix
             Random rand = new Random(9312);
             int n = 11, n3 = 1, n4 = 2;
             double[,] matrix_oriented = new double[100, 100];
-            double[,] matrix_non_oriented = new double[100, 100];
 
             for (int i = 0; i < n; i++)
                 for (int j = 0; j < n; j++)
-                    matrix_oriented[i,j] = Math.Floor((Convert.ToDouble(rand.Next(200))/100) * (1.0 - n3 * 0.02 - n4 * 0.005 - 0.25));
-
-            for (int i = 0; i < n; i++)
-                    for (int j = 0; j <= i; j++) 
-                    {
-                        matrix_non_oriented[i,j] = Math.Floor((Convert.ToDouble(rand.Next(200))/100) * (1.0 - n3 * 0.02 - n4 * 0.005 - 0.25));
-                        matrix_non_oriented[j, i] = matrix_non_oriented[i, j];
-                    }
+                    matrix_oriented[i,j] = Math.Floor((Convert.ToDouble(rand.Next(200))/100) * (1.0 - n3 * 0.01 - n4 * 0.005 - 0.15));
 
             WriteArray("Oriented", matrix_oriented);
             WriteGraph("Oriented", matrix_oriented);
-            WriteArray("Non-Oriented", matrix_non_oriented);
-            WriteGraph("Non-Oriented", matrix_non_oriented);
         }
     }
 }
