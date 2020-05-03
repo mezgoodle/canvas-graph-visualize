d39bc3126d791dd73a33ed4b6656504cdf0fb14d
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-05-03 15:13:25 +0300

    Update Program.cs

--- Lab_rob_5/matrix/Program.cs
+++ Lab_rob_5/matrix/Program.cs
@@ -45,11 +45,13 @@ namespace matrix
 
             // Only matrix
             for (int i = 0; i < n; i++)
-                for (int j = 0; j <= i; j++) 
-                {
+                for (int j = 0; j < n; j++) 
                     matrix_non_oriented[i,j] = Math.Floor((Convert.ToDouble(rand.Next(200))/100) * (1.0 - n3 * 0.01 - n4 * 0.005 - 0.05));
-                    matrix_non_oriented[j, i] = matrix_non_oriented[i, j];
-                }
+            
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    if (matrix_non_oriented[i,j] != 0)
+                        matrix_non_oriented[j,i] = matrix_non_oriented[i,j];
 
             // Create matrix of weights
             for (int i = 0; i < n; i++)
